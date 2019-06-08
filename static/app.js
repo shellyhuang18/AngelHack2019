fbd64a9f4661888cbe20170934f7833e323eb68e
Jack C <chen.jack@email.com>
2019-06-08 16:22:51 -0400

    stuff

--- static/app.js
+++ static/app.js
@@ -1,5 +1,15 @@
-console.log('fired');
-document.querySelector('#start').onclick = function() {
+console.log('Firing');
+let sentenceBuilder = ''
+let totalPlayers = 2
+let currentPlayer = 1  // index starts at 1
+let currentRound = 0
+const maxRounds = 5
+
+const POSSIBLE_GOALS = ['Anger', 'Fear', 'Joy', 'Sadness', 'Analytical', 'Confident', 'Tentative']
+
+newRound(currentRound)
+
+$('#start').on('click', function() {
     fetch('/api/speech-to-text/token')
       .then(function(response) {
         return response.text();
@@ -15,7 +25,9 @@ document.querySelector('#start').onclick = function() {
         });
 
         stream.on('data', data => {
-
+            if (true) {
+                firstSentenceCompleted(data)
+            }
         })
   
         document.querySelector('#stop').onclick = function() {
@@ -25,8 +37,56 @@ document.querySelector('#start').onclick = function() {
       .catch(function(error) {
         console.log(error);
       });
-  };
+  })
 
-function firstSentenceCompleted () {
+$('#next').on('click', nextPlayer)
+// Function Declarations
+
+function firstSentenceCompleted (data) {
+  sentenceBuilder += data
+  $('.story').text(sentenceBuilder)
+  nextPlayer()
+}
+
+function nextPlayer() {
+    if (currentPlayer + 1 >= totalPlayers) {
+        currentRound += 1
+        newRound(currentRound)
+    }
+    currentPlayer = (currentPlayer + 1) % totalPlayers
+}
+
+function newRound (round) {
+    console.log('Starting round', currentRound, '/', maxRounds);
+    $('.round-display').text(round)
+    if (currentRound === 0) {
+        // If First Round
+    } else if (currentRound === maxRounds) {
+        // If final round
+    } else {
+
+    }
+}
+
+function generateGoalPair () {
+    // Generate a unique pair
+    const pair = []
+    const firstVal = getRandomValue(POSSIBLE_GOALS)
+    pair.push(firstVal)
+    if (POSSIBLE_GOALS.length >= 2) {
+        let secondVal = getRandomValue(POSSIBLE_GOALS)
+        while (firstVal === secondVal) {
+
+        }
+        pair.push(secondVal)
+    }
+    return pair
+}
+
+function getRandomValue(arr) {
+    return arr[Math.floor(Math.random() * arr.length)]
+}
+
+function initMatch () {
 
 }
